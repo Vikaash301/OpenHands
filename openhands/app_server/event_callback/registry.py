ffb5bc81e3f1e555b0fb6829646459e94aa6d2af
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-12-01 15:29:46 -0500

    debug rebuilding models

--- openhands/app_server/event_callback/registry.py
+++ openhands/app_server/event_callback/registry.py
@@ -4,6 +4,8 @@ from openhands.app_server.event_callback.event_callback_models import (
     EventCallbackProcessor,
 )
 
+from openhands.sdk.utils.models import get_known_concrete_subclasses
+
 T = TypeVar("T", bound=EventCallbackProcessor)
 
 REGISTERED_EVENT_CALLBACK_PROCESSORS: dict[str, type[EventCallbackProcessor]] = {}
@@ -12,5 +14,11 @@ REGISTERED_EVENT_CALLBACK_PROCESSORS: dict[str, type[EventCallbackProcessor]] = 
 def register_event_callback_processor(cls: T) -> T:
     REGISTERED_EVENT_CALLBACK_PROCESSORS[cls.__name__] = cls
     # Force rebuild of discriminated union so Pydantic sees the new type
+
+    subclasses = get_known_concrete_subclasses(cls)
+
+    print('known classes', subclasses)
+    print([subclass.__name__ for subclass in subclasses])
+    print('rebuilding typing system')
     EventCallbackProcessor.model_rebuild(force=True)
     return cls
